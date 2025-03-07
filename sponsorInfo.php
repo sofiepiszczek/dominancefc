3c907f601bf171c1b2f5c2a03845092731891ddd
emmaprobst <emp003@edu.zealand.dk>
2025-03-07 21:16:32 +0100

    Er startet på "Sponsor Info" og har tilføjet tekst, video og knap.

---
+++ sponsorInfo.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Sponsor Info</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/30d6e0b537.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-secondary">
+
+
+
+
+
+<div class="container">
+    <div class="row text-center justify-content-center align-items-center">
+
+        <div class="mb-5">
+            <p class="h1 text-primary">SPONSOR INFO</p>
+        </div>
+
+
+        <div class="col-12 p-4 pb-5">
+            <iframe src="https://www.youtube.com/embed/ikyIMVTR-eA?si=RvKKVmGCMG921KoP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+
+        <div class="mb-5">
+            <p class="h1 text-white">BE OUR <br> CORNERMAN</p>
+        </div>
+
+
+        <div class="mb-5">
+            <button class="btn btn-primary rounded-3 px-5 py-2 text-white">CONTACT</button>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
