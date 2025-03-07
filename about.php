a6acdd88685429ce485ac6dadba4a8d5dea7b6a4
emmaprobst <emp003@edu.zealand.dk>
2025-03-07 09:57:12 +0100

    Er startet på about-siden uden tilføjelse af billeder og korrekt typografi

---
+++ about.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>About</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-secondary">
+
+
+<div class="container">
+    <div class="row justify-content-center align-items-center text-white text-center">
+        <div>
+            <h1 class="text-primary">ABOUT</h1>
+        </div>
+
+        <div>
+            <img src="" alt="Portræt af Mark O. Madsen">
+        </div>
+
+        <div class="text-start p-5">
+            <h4 class="text-primary">WHAT IS DOMINANCE FC?</h4>
+            <h5>Dominance FC, short for Dominance Fighting Championship, was established by three-time Olympian and UFC veteran Mark O. Madsen. Dominance FC’s vision is to establish Europe’s premier fight promotion, hosting multiple DFC events annually. Dominance FC is dedicated to advancing the sport of MMA in Europe, creating exceptional experiences for fighters and fans alike.</h5>
+        </div>
+
+        <div>
+            <img src="" alt="Billede af ringen">
+        </div>
+
+        <div class="text-start p-5">
+            <h4 class="text-primary">WHY DOMINANCE FC?</h4>
+            <h5>MMA, or Mixed Martial Arts, is the world’s fastest-growing sport, and we see enormous potential in the European MMA scene. Dominance FC aims to create a platform that matches this growth, promotes the sport in Europe, and provides European talents with an international stage to develop their potential and build their brand.</h5>
+        </div>
+
+        <div>
+            <img src="" alt="Billede af Mark O. Madsen">
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
