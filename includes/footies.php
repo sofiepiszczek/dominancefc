56bc67d937710031f7f9812611b3bdca1e7e981f
emmaprobst <emp003@edu.zealand.dk>
2025-03-10 14:48:24 +0100

    Har indsat links

--- includes/footies.php
+++ includes/footies.php
@@ -5,27 +5,27 @@
 
         <div class="row flex-wrap gap-2">
             <a class="h5 custom-text">EXPERIENCE</a>
-            <a href="#" class="h6">Tickets</a>
-            <a href="#" class="h6">Events</a>
-            <a href="#" class="h6">Payperview</a>
+            <a href="https://www.billetlugen.dk/artist/dominance-fc-3-0/?affiliate=P0G" class="h6">Tickets</a>
+            <a href="events.php" class="h6">Events</a>
+            <a href="ppv.php" class="h6">Payperview</a>
 
             <br>
 
             <a class="h5 custom-text">DISCOVER</a>
-            <a href="#" class="h6">Behind The Scenes</a>
-            <a href="#" class="h6">Previous Fights</a>
-            <a href="#" class="h6">Shop</a>
-            <a href="#" class="h6">Podcast</a>
-            <a href="#" class="h6">About</a>
+            <a href="behindTheScenes.php" class="h6">Behind The Scenes</a>
+            <a href="previousEvents.php" class="h6">Previous Events</a>
+            <a href="shop.php" class="h6">Shop</a>
+            <a href="podcast.php" class="h6">Podcast</a>
+            <a href="about.php" class="h6">About</a>
             <a href="#" class="h6">Cookie Policy (EU)</a>
             <a href="#" class="h6">Privacy Statement (EU)</a>
 
             <br>
 
             <a class="h5 custom-text">BE FAMILY</a>
-            <a href="#" class="h6">Contact</a>
-            <a href="#" class="h6">Fighter Signup</a>
-            <a href="#" class="h6">Sponsor Info</a>
+            <a href="contact.php" class="h6">Contact</a>
+            <a href="fighterSignup.php" class="h6">Fighter Signup</a>
+            <a href="sponsorInfo.php" class="h6">Sponsor Info</a>
 
             <br>
         </div>
