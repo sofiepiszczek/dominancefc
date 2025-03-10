e23c21bd2f4bead772c3a92eff8e3de78b20b9c4
Sofie Piszczek <sor004@edu.zealand.dk>
2025-03-10 09:28:59 +0100

    oprettet ppv

---
+++ ppv.php
@@ -0,0 +1,31 @@
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Payperview</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<h1 class="custom-text text-primary text-center py-5">PAYPERVIEW</h1>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="hero-section d-flex flex-column align-items-center justify-content-center text-white custom-text py-1">
+            <h2 class="py-5">WATCH DFC ONLINE AT PLUTO TV</h2>
+            <a class="btn bg-primary text-white" href="">WATCH DFC 3.0 HERE</a>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
