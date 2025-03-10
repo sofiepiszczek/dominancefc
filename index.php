c0828d15c120a824a4d7c55d72eb7813155343b8
Marc <mah035@edu.zealand.dk>
2025-03-10 14:42:38 +0100

    Footjob done

--- index.php
+++ index.php
@@ -102,7 +102,9 @@ include ("includes/navbar.php");
 
 <br><br><br><br><br><br>
 
-
+<?php
+include ("includes/footies.php");
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
