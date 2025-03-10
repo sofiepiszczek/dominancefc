23eb55b36d05a6ccdd029dd11ebb6c68f4822a62
Sofie Piszczek <sor004@edu.zealand.dk>
2025-03-10 10:49:02 +0100

    Lavet skelet

--- ppv.php
+++ ppv.php
@@ -20,8 +20,23 @@
 <div class="container-fluid">
     <div class="row">
         <div class="hero-section d-flex flex-column align-items-center justify-content-center text-white custom-text py-1">
-            <h2 class="py-5">WATCH DFC ONLINE AT PLUTO TV</h2>
-            <a class="btn bg-primary text-white" href="">WATCH DFC 3.0 HERE</a>
+            <h2 class="py-5 fs-1 text-center mt-5 text-white">WATCH DFC ONLINE AT PLUTO TV</h2>
+            <a class="btn bg-primary text-white fs-3" href="">WATCH DFC 3.0 HERE</a>
+        </div>
+    </div>
+</div>
+<div class="container pb-5">
+    <div class="row">
+        <div class="col-12 d-inline-flex justify-content-center">
+            <div class="col-7 py-5 my-5">
+                <img src="images/Plutopng.png" class="img-fluid" alt="Pluto Logo">
+            </div>
+        </div>
+        <div class="col-12 text-center text-white custom-text mb-4">
+            <h2 class="fs-1">DISCOVER<br><span class="fs-3">MORE FROM</span><br> DOMINANCE</h2>
+        </div>
+        <div class="col-12 text-center">
+            <a href="" class="btn bg-primary text-white custom-text py-2">CLICK HERE!</a>
         </div>
     </div>
 </div>
