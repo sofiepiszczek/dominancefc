40fd3e8622e13bd7468dc7c197406c7ae6e040b2
emmaprobst <emp003@edu.zealand.dk>
2025-03-10 08:52:35 +0100

    Har startet filen "shop" og indsat billeder, tekst og knapper.

---
+++ shop.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Shop</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/30d6e0b537.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body class="bg-secondary">
+
+
+
+
+
+<div class="container">
+    <div class="row text-center justify-content-center align-items-center">
+
+        <div class="mb-5">
+            <p class="h1 text-primary mb-5">SHOP</p>
+            <p class="h4 text-white mb-0">DRESS WITH</p>
+            <p class="h2 text-white">DOMINANCE!</p>
+        </div>
+
+        <div class="">
+            <img src="" alt="Billede af trøje">
+            <p class="h5 text-white">UNISEX BLACK DFC CAGE T-SHIRT</p>
+            <p class="h5 text-white">€30</p>
+            <a href="#" class="btn btn-primary text-white py-2 px-4">BUY</a>
+        </div>
+
+        <div class="">
+            <img src="" alt="Billede af trøje">
+            <p class="h5 text-white">UNISEX BLACK DFC CAGE T-SHIRT</p>
+            <p class="h5 text-white">€30</p>
+            <a href="#" class="btn btn-primary text-white py-2 px-4">BUY</a>
+        </div>
+
+        <div class="">
+            <img src="" alt="Billede af trøje">
+            <p class="h5 text-white">UNISEX BLACK DFC CAGE T-SHIRT</p>
+            <p class="h5 text-white">€30</p>
+            <a href="#" class="btn btn-primary text-white py-2 px-4">BUY</a>
+        </div>
+
+
+
+
+
+
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
