1f3cbdeb3fc9859b8a73f65618368df1fbfd7c88
emmaprobst <emp003@edu.zealand.dk>
2025-03-11 19:46:42 +0100

    Har tilføjet billeder og tekst

--- sponsorInfo.php
+++ sponsorInfo.php
@@ -32,24 +32,77 @@ include ("includes/navbar.php");
 
 
 <div class="container">
-    <div class="row text-center justify-content-center align-items-center">
+    <div class="row justify-content-center text-center">
 
         <div class="">
             <p class="h1 text-primary py-5 custom-text">SPONSOR <br> INFO</p>
         </div>
 
-
         <div class="col-12 px-4 pb-5">
             <iframe width="300" height="168" src="https://www.youtube.com/embed/ikyIMVTR-eA?si=RvKKVmGCMG921KoP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
-        <div class="mb-5">
-            <p class="h1 text-white custom-text">BE OUR <br> CORNERMAN</p>
+
+        <div class="col-md-6">
+            <p class="h3 text-white py-4 custom-text px-3">BENEFITS OF BEING AN AIRTOX DOMINANCE FC SPONSOR</p>
         </div>
 
 
+    <div class="row justify-content-center">
+        <div class="mt-3">
+            <div class="">
+                <img src="images/benefitSpons1.jpg" class="w-75 mb-4" alt="billede">
+            </div>
+
+            <div class="h6 text-white text-uppercase">
+                <p><span class="text-primary h5 custom-text">Gain Visibility</span> <br><br> Your business will be prominently featured at our MMA shows, on our social media platforms, and in our marketing materials.</p>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <div class="">
+                <img src="images/benefitSpons2.jpg" class="w-75 mb-4" alt="billede">
+            </div>
+
+            <div class="h6 text-white text-uppercase">
+                <p><span class="text-primary h5 custom-text">Network with Leaders</span> <br><br> Access to the “Dominance Fight Club”, where you can network with entrepreneurs, business leaders, and MMA enthusiasts.</p>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <div class="">
+                <img src="images/benefitSpons3.jpg" class="w-75 mb-4" alt="billede">
+            </div>
+
+            <div class="h6 text-white text-uppercase">
+                <p><span class="text-primary h5 custom-text">Exclusive Events</span> <br><br> Invitations to exclusive events and opportunities to meet fighters and other key figures in the MMA world.</p>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <div class="">
+                <img src="images/benefitSpons4.jpg" class="w-75 mb-4" alt="billede">
+            </div>
+
+            <div class="h6 text-white text-uppercase">
+                <p><span class="text-primary h5 custom-text">Brand Association</span> <br><br> Associate your brand with a dynamic and rapidly growing sport, showcasing your support for the MMA community and professional athletes.</p>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+    <div class="mb-5">
+            <p class="h1 text-white custom-text">BE OUR <br> CORNERMAN</p>
+        </div>
+
         <div class="mb-5">
-            <button class="btn btn-primary rounded-2 px-5 py-2 text-white custom-text">CONTACT</button>
+            <button class="btn btn-primary rounded-2 px-4 py-2 text-white custom-text">CONTACT</button>
         </div>
 
     </div>
