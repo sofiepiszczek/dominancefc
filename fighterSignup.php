0792cfc63e1669493e2f9f634d48f383bdff4cbc
emmaprobst <emp003@edu.zealand.dk>
2025-03-11 12:25:35 +0100

    Har gjort Fighter Signup responsiv til desktop

--- fighterSignup.php
+++ fighterSignup.php
@@ -31,50 +31,68 @@ include ("includes/navbar.php");
 ?>
 
 <div class="container">
-    <div class="row justify-content-center align-items-center">
-
-        <div class="text-center pb-4">
+    <div class="row justify-content-center text-center">
+        <div class="col-md-6">
             <p class="h1 text-primary custom-text py-5">FIGHTER <br> SIGNUP</p>
-            <p class="px-4">Apply for our fighter roster! <br> Do you have what it takes to perform at the top level, and compete with some of the best and most prominent fighters in the world?</p>
+            <p class="px-4 custom-text"><strong>Apply for our fighter roster!</strong></p>
+            <p class="px-4">Do you have what it takes to perform at the top level, and compete with some of the best and most prominent fighters in the world?</p>
         </div>
 
-        <div class="mb-3 px-4">
+
+    <div class="row justify-content-center text-start">
+        <div class="col-md-5 px-4 mt-5">
             <label class="form-label text-white custom-text fs-6 text-start">FULL NAME:</label>
             <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT FULL NAME HERE">
+        </div>
 
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">EXPERIENCE:</label>
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">EXPERIENCE:</label>
             <select class="form-select bg-white custom-text text-secondary placeholder-gray py-3 custom-placeholder" aria-label="Select your experience">
                 <option selected>SELECT YOUR EXPERIENCE</option>
                 <option value="1">PROFESSIONAL</option>
                 <option value="2">AMATEUR</option>
             </select>
-
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">DATE OF BIRTH:</label>
-            <input type="date" class="form-control bg-white custom-text text-secondary placeholder-gray py-3 custom-placeholder" placeholder="INSERT DATE OF BIRTH HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">E-MAIL:</label>
-            <input type="email" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT E-MAIL HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">PHONE NUMBER:</label>
-            <input type="number" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT NUMBER HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">NATIONALITY:</label>
-            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT NATIONALITY HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">WEIGHT CLASS:</label>
-            <input type="number" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT WEIGHT CLASS HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">FIGHT RECORD:</label>
-            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT FIGHT RECORD HERE">
-
-            <label class="form-label text-white custom-text mt-5 fs-6 text-start">AFFILIATED GYM AND COACH:</label>
-            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT HERE">
         </div>
 
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">DATE OF BIRTH:</label>
+            <input type="date" class="form-control bg-white custom-text text-secondary placeholder-gray py-3 custom-placeholder" placeholder="INSERT DATE OF BIRTH HERE">
+        </div>
 
-        <div class="w-75">
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">E-MAIL:</label>
+            <input type="email" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT E-MAIL HERE">
+        </div>
+
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">PHONE NUMBER:</label>
+            <input type="number" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT NUMBER HERE">
+        </div>
+
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">NATIONALITY:</label>
+            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT NATIONALITY HERE">
+        </div>
+
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">WEIGHT CLASS:</label>
+            <input type="number" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT WEIGHT CLASS HERE">
+        </div>
+
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">FIGHT RECORD:</label>
+            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT FIGHT RECORD HERE">
+        </div>
+
+        <div class="col-md-5 px-4 mt-5">
+            <label class="form-label text-white custom-text fs-6 text-start">AFFILIATED GYM AND COACH:</label>
+            <input type="text" class="form-control bg-white custom-text text-secondary placeholder-gray py-3" placeholder="INSERT HERE">
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center">
+        <div class="col-md-5 px-4 mt-5">
             <input type="text" class="form-control custom-text border-0 border-bottom bg-transparent text-white placeholder-gray text-center my-5 underline" style="box-shadow: none" placeholder="FACEBOOK LINK">
 
             <input type="text" class="form-control custom-text border-0 border-bottom bg-transparent text-white placeholder-gray text-center my-5 underline" style="box-shadow: none" placeholder="INSTAGRAM LINK">
@@ -85,29 +103,31 @@ include ("includes/navbar.php");
 
             <input type="text" class="form-control custom-text border-0 border-bottom bg-transparent text-white placeholder-gray text-center my-5 underline" style="box-shadow: none" placeholder="YOUTUBE LINK">
         </div>
+    </div>
 
-
-        <div class="py-3 text-center">
-            <p class="px-4">Do you allow us to save your info for future events and agree to our privacy policy, marketing, and tracking? You can withdraw consent anytime</p>
+    <div class="row justify-content-center">
+        <div class="col-md-4 px-4 mt-5">
+            <p class="px-4 fw-bold center">Do you allow us to save your info for future events and agree to our privacy policy, marketing, and tracking? You can withdraw consent anytime.</p>
         </div>
+    </div>
 
-
-        <div class="text-white text-center">
-            <div class="d-flex align-items-center my-4">
+    <div class="row justify-content-center">
+        <div class="col-md-4 text-white text-start">
+            <div class="d-flex align-items-center mt-5">
                 <input type="radio" id="yes" name="permission" class="form-check-input ms-5 p-3">
-                <label for="yes" class="fs-6 custom-text">YES, YOU HAVE MY PERMISSION.</label>
+                <label for="yes" class="ms-5 custom-text custom-fontsize">YES, YOU HAVE MY PERMISSION.</label>
             </div>
 
-            <div class="d-flex align-items-center my-3">
+            <div class="d-flex align-items-center my-4">
                 <input type="radio" id="no" name="permission" class="form-check-input ms-5 p-3">
-                <label for="no" class="fs-6 ms-5 custom-text">NO.</label>
+                <label for="no" class="ms-5 custom-text custom-fontsize">NO.</label>
             </div>
         </div>
-
 
         <div class="mb-5 mt-5 text-center">
             <button class="btn btn-primary rounded-2 px-5 py-2 text-white custom-text">SUBMIT</button>
         </div>
+    </div>
 
     </div>
 </div>
