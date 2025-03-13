c5460e7236ebba0a40f91a67dbd04090c3489ca9
Marc <mah035@edu.zealand.dk>
2025-03-13 13:10:09 +0100

    Footer and nav links fixed

--- includes/footies.php
+++ includes/footies.php
@@ -9,18 +9,18 @@
                     <div class="d-flex flex-column gap-2 text-start">
                         <a class="h5 custom-text">EXPERIENCE</a>
                         <a href="https://www.billetlugen.dk/artist/dominance-fc-3-0/?affiliate=P0G" target="_blank" class="h6 mb-2">Tickets</a>
-                        <a href="../events.php" class="h6 mb-2">Events</a>
-                        <a href="../ppv.php" class="h6 mb-2">Payperview</a>
+                        <a href="events.php" class="h6 mb-2">Events</a>
+                        <a href="ppv.php" class="h6 mb-2">Payperview</a>
                     </div>
                 </div>
 
                 <div class="col-lg-4 mb-5">
                     <div class="d-flex flex-column gap-2 text-start">
                         <a class="h5 custom-text">DISCOVER</a>
-                        <a href="../behindTheScenes.php" class="h6 mb-2">Behind The Scenes</a>
-                        <a href="../previousEvents.php" class="h6 mb-2">Previous Fights</a>
-                        <a href="../podcast.php" class="h6 mb-2">Podcast</a>
-                        <a href="../about.php" class="h6 mb-2">About</a>
+                        <a href="behindTheScenes.php" class="h6 mb-2">Behind The Scenes</a>
+                        <a href="previousEvents.php" class="h6 mb-2">Previous Fights</a>
+                        <a href="podcast.php" class="h6 mb-2">Podcast</a>
+                        <a href="about.php" class="h6 mb-2">About</a>
                         <a href="#" class="h6 mb-2">Cookie Policy (EU)</a>
                         <a href="#" class="h6 mb-2">Privacy Statement (EU)</a>
                     </div>
@@ -29,10 +29,10 @@
                 <div class="col-lg-4 mb-5">
                     <div class="d-flex flex-column gap-2 text-start">
                         <a class="h5 custom-text">BE FAMILY</a>
-                        <a href="../shop.php" class="h6 mb-2">Merch</a>
-                        <a href="../contact.php" class="h6 mb-2">Contact</a>
-                        <a href="../fighterSignup.php" class="h6 mb-2">Fighter Signup</a>
-                        <a href="../sponsorInfo.php" class="h6 mb-2">Sponsor Info</a>
+                        <a href="shop.php" class="h6 mb-2">Merch</a>
+                        <a href="contact.php" class="h6 mb-2">Contact</a>
+                        <a href="fighterSignup.php" class="h6 mb-2">Fighter Signup</a>
+                        <a href="sponsorInfo.php" class="h6 mb-2">Sponsor Info</a>
                     </div>
 
                     <div class="d-flex align-items-center gap-3 mt-4">
